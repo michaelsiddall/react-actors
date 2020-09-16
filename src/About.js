94afa8e44415b65a66bff85f59614d2270728bcd
Mike Siddall <eatingartist@gmail.com>
2020-09-15 20:15:19 -0500

    created and sourced Header and About files

---
+++ src/About.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+
+
+class About extends Component {
+
+
+    render() {
+        return(
+            <div>
+                <h2>About</h2>
+                <p>We are California's only improv group operating next to a nuclear reactor</p>
+            </div>
+        );
+    }
+
+
+}
+
+
+
+
+
+
+export default About;
